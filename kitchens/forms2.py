e4cc4e7ff83e4737fadd2b67f1cd7912ef795057
Vrajesh Shah <vrajeshshah13@gmail.com>
2021-12-13 20:31:02 -0500

    first commit

--- kitchens/forms2.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Review
-
-
-class ReviewsForm(forms.ModelForm):
-    class Meta:
-        model = Review
-        fields = '__all__'
--- kitchens/views.py
+++ kitchens/views.py
@@ -6,13 +6,19 @@ from actions.models import Action
 from .forms import KitchensForm
 from django.contrib import messages
 from django.http import JsonResponse
-
+import boto3
+from django.contrib.auth import authenticate
+from django.template.loader import render_to_string
 
 def index(request):
 
     actions = Action.objects.all()
 
+    
+
     return render(request, 'index.html', {'actions': actions})
+    
+    
 
 
 def addKitchen(request):
@@ -30,7 +36,32 @@ def addKitchen(request):
 
         form.save()
         messages.success(request, 'Form submission successful')
+    MTURK_SANDBOX = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
 
+    mturk = boto3.client('mturk',
+    aws_access_key_id = "AKIAZSMOGL4AWXD7N6U6",
+   aws_secret_access_key = "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",
+   region_name='us-east-1',
+   endpoint_url = MTURK_SANDBOX
+)
+    print("I have $" + mturk.get_account_balance()['AvailableBalance'] + " in my Sandbox account")
+
+    question = render_to_string('questions.xml' )
+    new_hit = mturk.create_hit(
+    Title = 'Is this Tweet happy, angry, excited, scared, annoyed or upset?',
+    Description = 'Read this tweet and type out one word to describe the emotion of the person posting it: happy, angry, scared, annoyed or upset',
+    Keywords = 'text, quick, labeling',
+    Reward = '0.15',
+    MaxAssignments = 1,
+    LifetimeInSeconds = 172800,
+    AssignmentDurationInSeconds = 600,
+    AutoApprovalDelayInSeconds = 14400,
+    Question = question,
+)
+
+    print("A new HIT has been created. You can preview it here:")
+    print("https://workersandbox.mturk.com/mturk/preview?groupId=" + new_hit['HIT']['HITGroupId'])
+    print("HITID = " + new_hit['HIT']['HITId'] + " (Use to Get Results)")
     
     context['form'] = form
     return render(request, 'addKitchen.html', context)
@@ -43,6 +74,9 @@ def viewKitchen(request):
     kitchens = Kitchen.objects.all()
 
     return render(request, 'viewKitchen.html', {'kitchens': kitchens})
+
+    
+
 
 
 def kitchenDetail(request, id):
